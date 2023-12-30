7de5c87b3ade8e5fa5f898e04658c3c3cdf69e00
naphtron <naphtron@yahoo.com>
2023-12-30 08:24:17 +0300

    added db

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -9,4 +13,5 @@ def index():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
