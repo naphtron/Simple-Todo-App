25c4f25584d61357897eac8a9811be899c061d55
naphtron <naphtron@yahoo.com>
2023-12-30 08:47:55 +0300

    added db

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 @app.route('/')
 def index():
     return render_template('base.html')
@@ -13,5 +18,6 @@ def index():
 
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
