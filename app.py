5b15740c0664d67c4cb7905a8b7314307a32ce6c
naphtron <naphtron@yahoo.com>
2024-01-02 06:12:00 +0300

    added update and delete functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
-#2023 issa wrap
+
 
 @app.route('/')
 def index():
@@ -19,7 +19,30 @@ def index():
     print(todo_list)
     return render_template('base.html', todo_list=todo_list)
 
+@app.route("/add", methods=['POST'])
+def add():
+    # add new item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # add new item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # add new item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     with app.app_context():
